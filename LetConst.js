9f3388cee2ff1cc9e835386cca6c67ac0f43245d
Safayat <safayathossain40@gmail.com>
2024-03-20 23:01:42 +0600

    Added

---
+++ LetConst.js
@@ -0,0 +1,18 @@
+const name ="Safayat Hossain";
+//name = "Forsaken God";
+console.log(name);
+const numbers =[1,2,4];
+numbers.push(1);
+const nayok ={
+    name:'safayat',
+    phone:'01772633182'
+}
+console.log(numbers);
+console.log(nayok);
+
+// let is a scope variable, leak is leak proof
+//var is not a scope variable
+let patientName ="Safayat";
+console.log(patientName);
+patientName="jason";
+console.log(patientName);
---
+++ template.js
@@ -0,0 +1,4 @@
+const firstName ="Safayat";
+const lastName ="Hossain";
+const fullName = firstName+" "+lastName;
+console.log(fullName);
