e093b4f1de10de2cf9a51962d7bb286f9f9e0967
Safayat <safayathossain40@gmail.com>
2024-03-21 03:18:02 +0600

    undefined vs null

---
+++ nullVsUndefined.js
@@ -0,0 +1,23 @@
+// let name ;
+// console.log(name);
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return num1+num2;
+// }
+// const result = add(1,2);
+// console.log(result);
+
+// function add1(num1,num2){
+//     console.log(num1,num2);
+// }
+// add1(1);
+
+// const premik ={
+//     name:"smart dude",
+//     phone:"2441139"
+// }
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
