702d37359529e199020cd06bab5c75f4a3cc83f2
Safayat <safayathossain40@gmail.com>
2024-03-21 01:02:44 +0600

    trueVsFalse

---
+++ ture_false.js
@@ -0,0 +1,16 @@
+const age = 9;
+if(age>0){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
+
+const name =" ";
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+let name1 = null;
+console.log(name1);
