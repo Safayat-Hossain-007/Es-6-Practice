7a585091a3a4adb23bb81ffa4c84f3093eb3dbf5
Safayat <safayathossain40@gmail.com>
2024-03-20 23:54:56 +0600

    oop concept

---
+++ class.js
@@ -0,0 +1,10 @@
+class Student{
+    constructor(id,name){
+        this.id = id;
+        this.name = name;
+    }
+}
+const student1 = new Student(1,"Safayat");
+const student2 = new Student(2,"Jake");
+const student3 = new Student(3,"Logan");
+console.log(student1.name,student2,student3);
